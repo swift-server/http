addb1029cf52382cdf2eef44d1da7f1bc25f42ae
seabaylea <baileyc@uk.ibm.com>
2017-05-31 15:47:49 +0100

    Update LICENCE and copyrights, add API.md

--- Tests/SwiftServerHttpTests/SwiftServerHttpTests.swift
+++ Tests/SwiftServerHttpTests/SwiftServerHttpTests.swift
@@ -1,3 +1,11 @@
+// This source file is part of the Swift.org Server APIs open source project
+//
+// Copyright (c) 2017 Swift Server API project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+//
+
 import XCTest
 
 @testable import SwiftServerHttp
