8f1f900474e078ca8931dc299c9553ea15a3ce97
Chris Bailey <baileyc@uk.ibm.com>
2017-08-13 08:03:55 +0100

    Add initial set of Jazzy docs (#24)

--- Sources/HTTP/HTTPRequest.swift
+++ Sources/HTTP/HTTPRequest.swift
@@ -9,27 +9,41 @@
 import Foundation
 import Dispatch
 
-/// HTTP Request NOT INCLUDING THE BODY. This allows for streaming
+/// A structure representing the headers from a HTTP request, without the body of the request.
 public struct HTTPRequest {
+    /// HTTP request method.
     public var method: HTTPMethod
-    public var target: String /* e.g. "/foo/bar?buz=qux" */
+    /// HTTP request URI, eg. "/foo/bar?buz=qux"
+    public var target: String
+    /// HTTP request version
     public var httpVersion: HTTPVersion
+    /// HTTP request headers
     public var headers: HTTPHeaders
 }
 
 /// Method that takes a chunk of request body and is expected to write to the ResponseWriter
-public typealias HTTPBodyHandler = (HTTPBodyChunk, inout Bool) -> Void /* the Bool can be set to true when we don't want to process anything further */
+/// - Parameter HTTPBodyChunk: `HTTPBodyChunk` representing some or all of the incoming request body
+/// - Parameter Bool: Bool that can be set to true in order to prevent further processing
+public typealias HTTPBodyHandler = (HTTPBodyChunk, inout Bool) -> Void
 
 /// Indicates whether the body is going to be processed or ignored
 public enum HTTPBodyProcessing {
-    case discardBody /* if you're not interested in the body */
+    /// Used to discard the body data associated with the incoming HTTP request
+    case discardBody
+    /// Used to process the body data associated with the imcoming HTTP request using a `HTTPBodyHandler`
     case processBody(handler: HTTPBodyHandler)
 }
 
-/// Part (or maybe all) of the incoming request body
+/// Part or all of the incoming request body
 public enum HTTPBodyChunk {
-    case chunk(data: DispatchData, finishedProcessing: () -> Void) /* a new bit of the HTTP request body has arrived, finishedProcessing() must be called when done with that chunk */
-    case failed(error: /*HTTPParser*/ Error) /* error while streaming the HTTP request body, eg. connection closed */
-    case trailer(key: String, value: String) /* trailer has arrived (this we actually haven't implemented yet) */
-    case end /* body and trailers finished */
+    /// A new chunk of the incoming HTTP reqest body data has arrived. `finishedProcessing()` must be called when
+    /// that chunk has been processed.
+    case chunk(data: DispatchData, finishedProcessing: () -> Void)
+    /// An error has occurred whilst streaming the incoming HTTP request data, eg. the connection closed
+    case failed(error: Error)
+    /// A trailer header has arrived during the processing of the incoming HTTP request data.
+    /// This is currently unimplemented.
+    case trailer(key: String, value: String)
+    /// The stream of incoming HTTP request data has completed.
+    case end
 }
