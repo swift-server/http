88ab09d04727d68a72a7f6fc829004fc750375d8
Helge Hess <me@helgehess.eu>
2017-11-05 23:51:45 +0100

    Drop HTTPServing
    The API is concrete, this protocol serves no purpose.

--- Sources/HTTP/HTTPServer.swift
+++ Sources/HTTP/HTTPServer.swift
@@ -6,26 +6,10 @@
 // See http://swift.org/LICENSE.txt for license information
 //
 
-/// Definition of an HTTP server.
-public protocol HTTPServing : class {
-
-    /// Start the HTTP server on the given `port`, using `handler` to process incoming requests
-    func start(port: Int, handler: @escaping HTTPRequestHandler) throws
-
-    /// Stop the server
-    func stop()
-
-    /// The port the server is listening on
-    var port: Int { get }
-
-    /// The number of current connections
-    var connectionCount: Int { get }
-}
-
 /// A basic HTTP server. Currently this is implemented using the PoCSocket
 /// abstraction, but the intention is to remove this dependency and reimplement
 /// the class using transport APIs provided by the Server APIs working group.
-public class HTTPServer: HTTPServing {
+public class HTTPServer {
     private let server = PoCSocketSimpleServer()
 
     /// Create an instance of the server. This needs to be followed with a call to `start(port:handler:)`
